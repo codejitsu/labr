4b613cdc8e0dfa5c45985a3082a6396101bf5412
Alex <me@codejitsu.net>
2018-10-14 13:21:45 +0200

    Added edge case

--- bot/test_greeting_intro.py
+++ bot/test_greeting_intro.py
@@ -8,6 +8,24 @@ class TestGreetingDialog(object):
     def test_greeting_intro(self, user, text):
         g = Greeting()
         reply, confidence = g.listen(text, user=user)
+
         assert confidence == 1.0
         assert reply is not None
         assert reply == "Hello, what is your name?"
+
+    @pytest.mark.xfail(reason="a case that must be handled")
+    @pytest.mark.parametrize("text", ["My name is Jake", "Hello, I'm Jake."])
+    @pytest.mark.parametrize("user", [None, "jkm"], ids = ["w/ user", "w/o user"])
+    def test_initial_intro(self, user, text):
+        g = Greeting()
+        reply, confidence = g.listen(text, user=user)
+
+        assert confidence == 1.0
+        assert reply is not None
+        assert reply == "Hello, Jake!"
+
+        if user is None:
+            user = "jake"
+
+        assert user in g.participants
+        assert g.participants[user] == 'Jake'

ba609a4057dd4fbcd7016410a328afddd4e5d80b
Alex <me@codejitsu.net>
2018-10-30 20:21:50 +0100

    Added simple aiml bot.

---
+++ bot/greeting_aiml.py
@@ -0,0 +1,25 @@
+from dialog import Dialog
+import aiml_bot
+
+class GreetingAiml(Dialog):
+    def __init__(self):
+        self.bot = aiml_bot.Bot(learn='greeting.aiml')
+
+    def parse(self, text):
+        return text
+
+    def interpret(self, sents, **kwargs):
+        res = self.bot.respond(sents)
+        if not res:
+            return res, 0.0, kwargs
+        else:
+            return res, 1.0, kwargs
+
+    def respond(self, sents, confidence, **kwargs):
+        return sents
+
+if __name__ == '__main__':
+    dialog = GreetingAiml()
+    dialog.bot.set_predicate('USERNAME', 'Alex')
+    print(dialog.listen("Hallo!")[0])
+    print(dialog.listen("Bis dann")[0])
--- bot/simple_bot.py
+++ bot/simple_bot.py
@@ -1,10 +1,10 @@
 from simple_conversation import SimpleConversation
-from greeting import Greeting
+from greeting_aiml import GreetingAiml
 import json
 import random
 
 if __name__ == '__main__':
-    dialogs = [Greeting()]
+    dialogs = [GreetingAiml()]
     conversation = SimpleConversation(dialogs)
 
     lang = 'de'
@@ -20,9 +20,10 @@ if __name__ == '__main__':
             response, confidence = conversation.listen(line)
 
             if confidence == 1.0:
-                print('BOT: {}'.format(response))
+                print('BOT: {} (confidence: {})'.format(response, confidence))
             else:
-                print('BOT: {}'.format(random.choice(templates[lang]['dont-understand-not-sure'])))
+                print('BOT: {} (confidence: {})'.format(random.choice(templates[lang]['dont-understand-not-sure']),
+                confidence))
         except EOFError:
             print("Bye!")
             sys.exit(0)
